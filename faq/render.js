6ba6dd1db6153b70d41d60d771796e2ad89fdd8e
Ben Parsons <ben@bpulse.co.uk>
2018-06-04 16:00:18 +0100

    little tool for layout and thinknig

---
+++ faq/render.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+var MarkdownIt = require('markdown-it');
+var md = new MarkdownIt();
+
+var style = fs.readFileSync("style.css", 'utf-8');
+console.log("<style>");
+console.log(style);
+console.log("</style>");
+
+fs.readdirSync(".").forEach(filename => {
+  if (filename.indexOf('section') === 0) {
+    //console.log(filename);
+    var file = fs.readFileSync(filename, 'utf-8');
+    console.log("<div>");
+    console.log(md.render(file));
+    console.log("</div>");
+  }
+})
